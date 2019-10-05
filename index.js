6eb8c7f20467c7cf1e35bed93456446d1cc531f3
oliver-wilson-dev <contact.oliver.wilson@gmail.com>
2019-10-05 10:04:21 +0100

    refactor: remove documentation comments

--- index.js
+++ index.js
@@ -4,12 +4,5 @@ const reviewSubmitted = require('./actions/PullRequests/reviewSubmitted');
 module.exports = (app) => {
   app.on('pull_request.opened', open);
 
-  // Running app when Pull Request Review is submitted
   app.on('pull_request_review.submitted', reviewSubmitted);
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
